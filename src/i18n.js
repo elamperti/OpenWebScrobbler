65864990ededb514ea6a607d0bebd2c97e95325f
Enrico Lamperti <elamperti@users.noreply.github.com>
2018-09-17 12:58:42 -0300

    Fix minor errors in Polish translation

--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@ export const languageList = [
   {code: 'ca', name: 'Català'},
   {code: 'en', name: 'English'},
   {code: 'es', name: 'Español'},
-  {code: 'pl', name: 'Polski'},
+  {code: 'pl', name: 'polski'},
   // {code: 'pt', name: 'Português'},
 ];
 
