e7de8473481eddf11fbdf36ac4e2caedbeee601b
Enrico Lamperti <elamperti@users.noreply.github.com>
2018-10-06 15:10:02 -0300

    Remove debug line

--- src/store/reducers/updatesReducer.js
+++ src/store/reducers/updatesReducer.js
@@ -5,7 +5,6 @@ const initialState = {
 const updatesReducer = (state=initialState, action) => {
   switch (action.type) {
     case 'NEW_VERSION_READY':
-      console.log('NEW VERSION READY FIRED');
       return {
         ...initialState,
         newVersionReady: true,
