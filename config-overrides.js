a06b4495abb8074415f3fef0f6a933eaa42f2e59
Enrico Lamperti <elamperti@users.noreply.github.com>
2018-10-06 14:33:41 -0300

    Fix react-dom version and force minified files from CDN

--- config-overrides.js
+++ config-overrides.js
@@ -12,50 +12,32 @@ function createDynamicCdnObject(moduleName, version, globalVarName, url) {
 }
 
 module.exports = function override(config, env) {
-  config.plugins = [
-    ...config.plugins,
-    new DynamicCdnWebpackPlugin({
-      verbose: !!process.env.LIST_CDN_BUNDLES,
-      resolver(moduleName, version) {
-        var url;
-        var subModuleName;
+  if (process.env.NODE_ENV !== 'development') {
+    config.plugins = [
+      ...config.plugins,
+      new DynamicCdnWebpackPlugin({
+        verbose: !!process.env.LIST_CDN_BUNDLES,
+        options: {
 
-        switch (moduleName) {
-      //     // case 'reactstrap':
-      //     //   url = `https://cdnjs.cloudflare.com/ajax/libs/reactstrap/${version}/reactstrap.min.js`;
-      //     //   return createDynamicCdnObject(moduleName, version, 'reactstrap', url);
+        },
+        resolver(moduleName, version) {
+          var url;
+          var subModuleName;
 
-      //     // case 'i18n':
-      //     case 'i18next':
-      //       url = `https://cdnjs.cloudflare.com/ajax/libs/i18next/${version}/i18next.min.js`;
-      //       return createDynamicCdnObject(moduleName, version, 'i18next', url);
+          switch (moduleName) {
+            case '@sentry/browser':
+              url = `https://browser.sentry-cdn.com/${version}/bundle.min.js`;
+              return createDynamicCdnObject(moduleName, version, 'Sentry', url);
 
-      //     // case 'i18next-xhr-backend':
-      //     //   url = `https://cdnjs.cloudflare.com/ajax/libs/i18next-xhr-backend/${version}/i18nextXHRBackend.min.js`;
-      //     //   return createDynamicCdnObject(moduleName, version, null, url);
+            default:
+              return moduleToCdn(moduleName, version, {env: 'production'});
+          };
+        },
+      }),
+    ];
+  }
 
-      //     case 'bootswatch/dist/slate/bootstrap.min.css':
-      //       url = `https://unpkg.com/bootswatch@${version}/dist/slate/bootstrap.min.css`;
-      //       return createDynamicCdnObject(moduleName, version, null, url);
-
-          case '@sentry/browser':
-            url = `https://browser.sentry-cdn.com/${version}/bundle.min.js`;
-            return createDynamicCdnObject(moduleName, version, 'Sentry', url);
-
-      //     // Probably a bad idea:
-      //     // case (moduleName.match(/^date-fns\//) || {}).input:
-      //     //   subModuleName = /^date-fns\/(.+)/.exec(moduleName)[1];
-      //     //   url = `https://unpkg.com/date-fns@${version}/${subModuleName}/index.js`;
-      //     //   return createDynamicCdnObject(moduleName, version, null, url);
-
-          default:
-            return moduleToCdn(moduleName, version);
-        };
-      },
-    }),
-  ];
-
-  if (env.ANALYZE_BUNDLE) {
+  if (process.env.ANALYZE_BUNDLE) {
     config.plugins.push(new BundleAnalyzerPlugin());
   }
 
