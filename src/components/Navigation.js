616573048436e80fb5845743af3550dcb8c5e734
Enrico Lamperti <elamperti@users.noreply.github.com>
2019-02-15 15:47:48 -0300

    Avoid error in navbar when path is missing
    Fixes OPENSCROBBLER-2E

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,7 @@ import { translate } from 'react-i18next';
 import i18n, { languageList } from 'i18n'; // just for the lang switcher
 import { connect } from 'react-redux';
 import ReactGA from 'react-ga';
+import get from 'lodash/get';
 
 import { Link } from 'react-router-dom';
 import {
@@ -89,7 +90,7 @@ class Navigation extends Component {
       return {
         tag: Link,
         to: dest,
-        className: this.props.location.pathname.startsWith(dest) ? 'active' : '',
+        className: get(this.props.location, 'pathname', '').startsWith(dest) ? 'active' : '',
       };
     }
 
