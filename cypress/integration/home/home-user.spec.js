77ec46260edbca40fe1dac6fe2d7964e011133ae
Enrico Lamperti <elamperti@users.noreply.github.com>
2020-04-16 20:00:06 -0300

    Fix Home details

--- cypress/integration/home/home-user.spec.js
+++ cypress/integration/home/home-user.spec.js
@@ -1,4 +1,4 @@
-describe("Home", function() {
+describe('Home (authenticated user)', function() {
   beforeEach(function() {
     cy.fixture('api/v2/user/visitor.json').as('userIsVisitorJSON')
     cy.fixture('api/v2/user/authenticated.json').as('userLoggedInJSON')
@@ -9,19 +9,19 @@ describe("Home", function() {
     cy.clearCookies()
   })
 
-  it("redirects to song scrobble after validating a token", function() {
+  it('redirects to song scrobble after validating a token', function() {
     cy.route('POST', `/api/v2/user.php`, '@userIsVisitorJSON').as('userData')
     cy.route('POST', `/api/v2/callback.php`, '@callbackSuccessJSON').as('callback')
 
-    cy.visit("/?token=aTestValue-withNumbers1234and_x1")
+    cy.visit('/?token=aTestValue-withNumbers1234and_x1')
     cy.wait(['@callback', '@userData'])
     cy.location('pathname').should('equal', '/scrobble/song')
   })
 
-  it("shows the user version of the home page", function() {
+  it('shows the user version of the home page', function() {
     cy.route('POST', `/api/v2/user.php`, '@userLoggedInJSON').as('userData')
 
-    cy.visit("/")
+    cy.visit('/')
     cy.wait(['@userData'])
     cy.get('[data-cy="HomeUser"]')
   })
--- cypress/integration/home/home-visitor.spec.js
+++ cypress/integration/home/home-visitor.spec.js
@@ -1,4 +1,4 @@
-describe("Home", function() {
+describe('Home (visitor)', function() {
   beforeEach(function() {
     cy.fixture('api/v2/user/visitor.json').as('userIsVisitorJSON')
     cy.fixture('api/v2/callback/failure.json').as('callbackFailureJSON')
@@ -6,21 +6,21 @@ describe("Home", function() {
     cy.server()
     cy.route('POST', `/api/v2/user.php`, '@userIsVisitorJSON')
 
-    cy.visit("/")
+    cy.visit('/')
   })
 
-  it("shows the visitor version of the home page", function() {
+  it('shows the visitor version of the home page', function() {
     cy.get('[data-cy="HomeVisitor"]')
   })
 
-  it("is using translations", function() {
-    cy.get('[data-cy="HomeVisitor-about"] p').should("not.contain", "about.description")
+  it('is using translations', function() {
+    cy.get('[data-cy="HomeVisitor-about"] p').should('not.contain', 'about.description')
   })
 
-  it("shows an error if token was invalid", function() {
+  it('shows an error if token was invalid', function() {
     cy.route('POST', `/api/v2/callback.php`, '@callbackFailureJSON')
 
-    cy.visit("/?token=aTestValue-withNumbers1234and_x1")
+    cy.visit('/?token=aTestValue-withNumbers1234and_x1')
     cy.get('.alert-danger').should('exist')
   })
 })
