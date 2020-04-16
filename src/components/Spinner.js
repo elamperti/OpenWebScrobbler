cbcd9787829898ac653cc1cfabcb9edac0cc0bc3
Enrico Lamperti <elamperti@users.noreply.github.com>
2020-04-16 19:33:20 -0300

    Add a rudimentary loading spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Trans } from 'react-i18next'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faCompactDisc,
-} from '@fortawesome/free-solid-svg-icons'
+import { faCompactDisc } from '@fortawesome/free-solid-svg-icons'
 
 const Spinner = () => {
   return (
     <div className="text-center mt-5 mx-auto">
       <FontAwesomeIcon icon={faCompactDisc} color="var(--gray-light)" size="4x" spin />
-      <br />
-      <p className="mt-1">
+      <p className="mt-2">
         <Trans i18nKey="loading">Loading...</Trans>
       </p>
     </div>
