172c788a98f2d823479b299c1c5db4282e41258e
Enrico Lamperti <elamperti@users.noreply.github.com>
2020-05-17 12:18:28 -0300

    Fix home files naming

---
+++ src/domains/home/Home.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import HomeUser from 'domains/home/HomeUser';
+import HomeVisitor from 'domains/home/HomeVisitor';
+
+import './Home.scss';
+
+const bodyDecoration = 'with-shadow';
+
+export default function Home() {
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+
+  useEffect(() => {
+    // Add body class for special home background
+    // ToDo: get rid of this unholy hack
+    document.body.classList.add(bodyDecoration);
+
+    return () => { // unmount
+      document.body.classList.remove(bodyDecoration);
+    };
+  });
+
+  return isLoggedIn ? <HomeUser /> : <HomeVisitor />;
+}
