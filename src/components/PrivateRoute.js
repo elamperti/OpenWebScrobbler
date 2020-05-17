70bd41aab5e21fc94d9146d29b06134e5e500f62
Enrico Lamperti <elamperti@users.noreply.github.com>
2020-05-17 12:20:11 -0300

    Refactor PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-class PrivateRoute extends React.Component {
-  render() {
-    if (this.props.user.isLoggedIn) {
-      return <Route {...this.props} />;
-    } else if (this.props.user.isLoggedIn === false) {
-      return <Redirect to="/" />;
-    } else {
-      return null;
-    }
+export default function PrivateRoute(props) {
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+
+  if (isLoggedIn) {
+    return <Route {...props} />;
+  } else {
+    return <Redirect to="/" />;
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
-
-const mapDispatchToProps = () => {
-  return {};
-};
-
-PrivateRoute.propTypes = {
-  user: PropTypes.shape({
-    isLoggedIn: PropTypes.bool,
-  }).isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PrivateRoute);
