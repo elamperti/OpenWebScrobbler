16f66984247ca360fbf924f39166d48ce3444c43
Enrico Lamperti <elamperti@users.noreply.github.com>
2021-03-17 01:55:17 -0300

    Refactor time picker

--- src/components/DateTimePicker.js
+++ src/components/DateTimePicker.js
@@ -1,123 +1,132 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Trans, useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 import addDays from 'date-fns/add_days';
 import subDays from 'date-fns/sub_days';
 import format from 'date-fns/format';
 
-import { Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import { Input, InputGroup, InputGroupAddon, Modal, ModalBody, ModalHeader } from 'reactstrap';
+import TimeKeeper from 'react-timekeeper';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faCalendarAlt, faClock } from '@fortawesome/free-regular-svg-icons';
 
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
-import TimePicker from 'components/TimePicker';
 
-class DatePickerInput extends React.Component {
-  render() {
-    return (
-      <InputGroup size="sm">
-        <InputGroupAddon addonType="prepend">
-          <span className="input-group-text">
-            <FontAwesomeIcon icon={faCalendarAlt} />
-          </span>
-        </InputGroupAddon>
-        <Input bsSize="sm" {...this.props} />
-      </InputGroup>
-    );
-  }
+import './DateTimePicker.scss';
+
+function DatePickerInput(props = {}) {
+  return (
+    <InputGroup size="sm">
+      <InputGroupAddon addonType="prepend">
+        <span className="input-group-text">
+          <FontAwesomeIcon icon={faCalendarAlt} />
+        </span>
+      </InputGroupAddon>
+      <Input bsSize="sm" {...props} />
+    </InputGroup>
+  );
 }
 
-// ToDo: turn into stateless component
-class DateTimePicker extends React.Component {
-  constructor(props) {
-    super(props);
+export default function DateTimePicker({ className = '', onChange, value, visible = false }) {
+  const use12Hours = useSelector((state) => state.settings.use12Hours);
+  const { t } = useTranslation();
+  const [timePickerModalVisible, setTimePickerModalVisible] = useState(false);
+  const showTimePicker = () => setTimePickerModalVisible(true);
+  const hideTimePicker = () => setTimePickerModalVisible(false);
 
-    this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleTimeChange = this.handleTimeChange.bind(this);
-  }
+  if (!visible) return null;
 
-  handleDateChange(timestamp) {
+  const minDate = subDays(new Date(), 14);
+  const maxDate = addDays(new Date(), 14);
+  const timeFormat = use12Hours ? 'h:mm A' : 'H:mm';
+
+  const handleDateChange = (timestamp) => {
     // Restore hours and minutes
-    const previousTimestamp = this.props.value;
-    timestamp.setHours(previousTimestamp.getHours());
-    timestamp.setMinutes(previousTimestamp.getMinutes());
+    timestamp.setHours(value.getHours());
+    timestamp.setMinutes(value.getMinutes());
 
-    this.props.onChange(timestamp);
-  }
-
-  handleTimeChange(timestamp) {
-    this.props.onChange(timestamp);
-  }
-
-  render() {
-    const t = this.props.t;
-    const minDate = subDays(new Date(), 14);
-    const maxDate = addDays(new Date(), 14);
-
-    return (
-      <div className={`timestamp row ${!this.props.visible && 'd-none'} ${this.props.className}`}>
-        <div className="col-sm-6 mt-3 pr-0">
-          <DayPickerInput
-            dayPickerProps={{
-              fromMonth: minDate,
-              toMonth: maxDate,
-              disabledDays: {
-                before: minDate,
-                after: maxDate,
-              },
-            }}
-            format={t('dates.format.short')}
-            formatDate={format}
-            component={DatePickerInput}
-            onDayChange={this.handleDateChange}
-            value={this.props.value}
-            inputProps={{ readOnly: true }}
-          />
-        </div>
-        <div className="col-sm-6 mt-3">
-          <TimePicker
-            use12Hours={!!this.props.settings.use12Hours}
-            onChange={this.handleTimeChange}
-            value={this.props.value}
-            format={this.props.settings.use12Hours ? 'hh:mm a' : 'HH:mm'}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    settings: state.settings,
+    onChange(timestamp);
   };
-};
 
-const mapDispatchToProps = () => {
-  return {};
-};
+  const handleTimeChange = (newTime) => {
+    const timestamp = new Date(value);
+    timestamp.setHours(newTime.hour);
+    timestamp.setMinutes(newTime.minute);
+    onChange(timestamp);
+  };
+
+  return (
+    <div className={`DateTimePicker mb-2 row ${className}`}>
+      <div className="col-sm-6 mt-3">
+        <DayPickerInput
+          dayPickerProps={{
+            fromMonth: minDate,
+            toMonth: maxDate,
+            disabledDays: {
+              before: minDate,
+              after: maxDate,
+            },
+          }}
+          format={t('dates.format.short')}
+          formatDate={format}
+          component={DatePickerInput}
+          onDayChange={handleDateChange}
+          value={value}
+          inputProps={{ readOnly: true, className: 'text-center' }}
+        />
+      </div>
+      <div className="col-sm-6 mt-3">
+        <InputGroup size="sm" id="TimePickerInputGroup">
+          <InputGroupAddon addonType="prepend">
+            <span className="input-group-text">
+              <FontAwesomeIcon icon={faClock} />
+            </span>
+          </InputGroupAddon>
+          <Input
+            id="TimePicker"
+            className="text-center"
+            bsSize="sm"
+            onClick={showTimePicker}
+            value={format(value, timeFormat)}
+            readOnly
+          />
+        </InputGroup>
+        <Modal
+          contentClassName="m-auto w-auto"
+          isOpen={timePickerModalVisible}
+          toggle={hideTimePicker}
+          centered={true}
+          scrollable={false}
+          fade={false}
+        >
+          <ModalHeader className="border-0 pt-2 pb-1" toggle={hideTimePicker}>
+            <Trans i18nKey="customTimestamp">Custom timestamp</Trans>
+          </ModalHeader>
+          <ModalBody>
+            <TimeKeeper
+              hour24Mode={!use12Hours}
+              time={format(value, 'H:mm')}
+              switchToMinuteOnHourSelect={true}
+              switchToMinuteOnHourDropdownSelect={true}
+              closeOnMinuteSelect={true}
+              onChange={handleTimeChange}
+              onDoneClick={hideTimePicker}
+              doneButton={() => null}
+            />
+          </ModalBody>
+        </Modal>
+      </div>
+    </div>
+  );
+}
 
 DateTimePicker.propTypes = {
   className: PropTypes.string,
   value: PropTypes.instanceOf(Date).isRequired,
+  onChange: PropTypes.func,
   visible: PropTypes.bool,
 };
-
-DateTimePicker.defaultProps = {
-  className: '',
-  visible: false,
-};
-
-DateTimePicker.propTypes = {
-  onChange: PropTypes.func,
-  t: PropTypes.func,
-  settings: PropTypes.shape({
-    use12Hours: PropTypes.bool,
-  }),
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(withTranslation()(DateTimePicker));
