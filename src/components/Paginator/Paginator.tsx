ef209eea8a94f43c3c9c44abc4fdbd08c06e28e7
Christian Musa <crash7@users.noreply.github.com>
2022-12-03 18:57:59 +0100

    Migrate Paginator to typescript

--- src/components/Paginator/Paginator.tsx
+++ src/components/Paginator/Paginator.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 const MAX_PAGE_ITEMS = 7; // ToDo: make it dynamic by viewport and/or currentPage digits' count
 
-export default function Paginator({ currentPage, pageCount, onPageChange }) {
+type PaginatorProps = {
+  currentPage: number;
+  pageCount: number;
+  onPageChange: (page: number) => void;
+};
+
+export default function Paginator({ currentPage, pageCount, onPageChange }: PaginatorProps) {
   const minBound = Math.max(currentPage - Math.floor(MAX_PAGE_ITEMS / 2), 1);
   const maxBound = Math.min(minBound + MAX_PAGE_ITEMS, pageCount);
 
-  const goToPage = (e) => {
-    const targetPage = e.currentTarget.dataset.page;
-    onPageChange(parseInt(targetPage));
+  const goToPage = (e: React.SyntheticEvent<HTMLElement>) => {
+    const targetPage = e.currentTarget.dataset.page as string;
+    onPageChange(parseInt(targetPage, 10));
   };
 
   const pageItems = Array(maxBound - minBound)
-    .fill()
+    .fill(0)
     .map((_, i) => minBound + i);
 
   return (
@@ -52,9 +57,3 @@ export default function Paginator({ currentPage, pageCount, onPageChange }) {
     </Pagination>
   );
 }
-
-Paginator.propTypes = {
-  currentPage: PropTypes.number,
-  pageCount: PropTypes.number,
-  onPageChange: PropTypes.func,
-};
