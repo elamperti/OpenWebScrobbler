8c3b9b851f5f2ad33fdf5a0362c64da3b57b568d
Enrico Lamperti <elamperti@users.noreply.github.com>
2023-02-12 18:40:28 -0300

    Remove data-cy attributes only for production builds

--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,34 @@
+const babelPlugins = [];
 const webpackPlugins = [];
 const isDevEnvironment = process.env.NODE_ENV === 'development';
+const isProdEnvironment = process.env.NODE_ENV === 'production';
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 if (process.env.ANALYZE_BUNDLE) {
   webpackPlugins.push(new BundleAnalyzerPlugin());
 }
 
+// Add LocatorJS in development
+babelPlugins.push([
+  '@locator/babel-jsx/dist',
+  {
+    env: 'development',
+  },
+]);
+
+// Remove data-cy attribute from elements
+if (isProdEnvironment) {
+  babelPlugins.push([
+    'babel-plugin-jsx-remove-data-test-id',
+    {
+      attributes: 'data-cy',
+    },
+  ]);
+}
+
 module.exports = {
   babel: {
-    plugins: [
-      [
-        '@locator/babel-jsx/dist',
-        {
-          env: 'development',
-        },
-      ],
-      [
-        'babel-plugin-jsx-remove-data-test-id',
-        {
-          attributes: 'data-cy',
-        },
-      ],
-    ],
+    plugins: babelPlugins,
   },
   webpack: {
     plugins: webpackPlugins,
