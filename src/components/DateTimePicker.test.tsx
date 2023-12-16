2bf29984220fa3f6fc6569cadcdac54036b45ec6
Enrico Lamperti <elamperti@users.noreply.github.com>
2023-12-15 21:20:28 -0300

    Add useless test for DateTimePicker component

---
+++ src/components/DateTimePicker.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import DateTimePicker from './DateTimePicker';
+import i18n from '../utils/i18n';
+
+const use12Hours = false;
+vi.mock('react-redux', async() => ({
+  ...(await vi.importActual('react-router-dom')),
+  useSelector: () => ({ settings: { use12Hours } }),
+}));
+
+describe('DateTime Picker', () => {
+  beforeEach(() => {
+    i18n.init();
+  });
+
+  it('renders correctly', () => {
+    const dummyDate = new Date();
+    const onChange = vi.fn();
+    render(<DateTimePicker onChange={onChange} value={dummyDate} />);
+
+    expect(onChange).not.toHaveBeenCalled();
+  });
+
+  // ToDo: test date/time changes? Maybe with Cypress is easier
+});
