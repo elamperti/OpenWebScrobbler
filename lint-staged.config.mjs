b6a489d26b2a0b1a2366975fa14df029f8a2632e
Enrico Lamperti <elamperti@users.noreply.github.com>
2023-12-20 21:03:19 -0300

    Check types pre-commit

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -4,6 +4,7 @@ export default {
   'src/**/*.{js,jsx,ts,tsx}': [
     'eslint --fix',
     () => 'yarn test:unit --silent',
+    () => 'yarn typecheck',
   ],
   'cypress/**/*': () => [
     'yarn test:e2e:quiet',
