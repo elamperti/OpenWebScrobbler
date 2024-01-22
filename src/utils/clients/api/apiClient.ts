c180a621655433f88d3ae48e9ee20f3dca9f5df7
Enrico Lamperti <elamperti@users.noreply.github.com>
2024-01-22 15:45:26 -0300

    Improve error handling for callback responses

--- src/utils/clients/api/apiClient.ts
+++ src/utils/clients/api/apiClient.ts
@@ -3,4 +3,8 @@ import { OPENSCROBBLER_API_URL } from 'Constants';
 
 export const openscrobblerAPI = axios.create({
   baseURL: OPENSCROBBLER_API_URL,
+  validateStatus: (statusCode: number) => {
+    if (statusCode === 503) return true;
+    return statusCode >= 200 && statusCode < 300;
+  },
 });
