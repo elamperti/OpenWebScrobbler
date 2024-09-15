5be2111922f3d47829348ff657c8db0a537edb2d
Enrico Lamperti <elamperti@users.noreply.github.com>
2024-09-15 18:28:52 -0300

    Remove unused stuff

--- src/domains/scrobbleAlbum/partials/TracklistFilter.tsx
+++ src/domains/scrobbleAlbum/partials/TracklistFilter.tsx
@@ -1,16 +1,15 @@
 import { ChangeEventHandler, useCallback, useContext, useState } from 'react';
 import { Trans } from 'react-i18next';
 import { Input } from 'reactstrap';
-import { faFilter, faX } from '@fortawesome/free-solid-svg-icons';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CleanupPatternContext, strToCleanupPattern } from '../CleanupContext';
 
 import './TracklistFilter.scss';
 
 export function TracklistFilter() {
-  const [showCleanupPatternCopy, setShowCleanupPatternCopy] = useState(false);
   const [showForm, setFormVisible] = useState(false);
-  const { cleanupPattern, setCleanupPattern } = useContext(CleanupPatternContext);
+  const { setCleanupPattern } = useContext(CleanupPatternContext);
   const [value, setValue] = useState('');
 
   const handleCleanupPatternChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
