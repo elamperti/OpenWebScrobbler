74188a85a0a97fdb802df47ebffbbac0d54bfa23
Alessandro Rabitti <silversonicaxel@users.noreply.github.com>
2024-09-15 23:59:52 +0200

    Add types to hooks (#245)

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -1,12 +1,24 @@
 import { useEffect } from 'react';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { UseMutateFunction, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { userGetSettings } from 'utils/clients/api/methods/userGetSettings';
 import { settingsUpdate } from 'utils/clients/api/methods/settingsUpdate';
 import type { Settings } from 'utils/types/settings';
 import { useLanguage } from './useLanguage';
 import { useUserData } from './useUserData';
 
-export const useSettings = () => {
+export type UseSettingsResult = {
+  settings: Settings;
+  updateSettings: UseMutateFunction<Settings, Error, Partial<Settings>, unknown>;
+  isLoading: boolean;
+  isError: boolean;
+  isFetching: boolean;
+  isReady: boolean;
+  isSuccess: boolean;
+};
+
+export type UseSettingsHook = () => UseSettingsResult;
+
+export const useSettings: UseSettingsHook = () => {
   const queryClient = useQueryClient();
   const { currentLanguage, setLanguage } = useLanguage();
   const { isReady: userIsReady, isLoggedIn } = useUserData();
@@ -38,9 +50,9 @@ export const useSettings = () => {
   return {
     settings: data,
     updateSettings: saveSettings.mutate,
+    isLoading,
     isError,
     isFetching,
-    isLoading,
     isReady: isFetched,
     isSuccess,
   };
