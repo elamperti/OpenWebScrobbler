9ab7a9f8757eed626b281bb1e27a126918c0e496
Enrico Lamperti <elamperti@users.noreply.github.com>
2025-01-05 16:55:28 -0300

    Add version header to outgoing API requests

--- src/utils/clients/api/apiClient.ts
+++ src/utils/clients/api/apiClient.ts
@@ -3,6 +3,9 @@ import { OPENSCROBBLER_API_URL } from 'Constants';
 
 export const openscrobblerAPI = axios.create({
   baseURL: OPENSCROBBLER_API_URL,
+  headers: {
+    'OWS-Version': process.env.REACT_APP_VERSION || '',
+  },
   validateStatus: (statusCode: number) => {
     if (statusCode === 503) return true;
     return statusCode >= 200 && statusCode < 300;
