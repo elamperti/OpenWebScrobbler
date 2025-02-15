3c466990f8f422056376e3bfaf8c2c3d1091be4f
Enrico Lamperti <elamperti@users.noreply.github.com>
2025-02-15 16:46:06 -0300

    Add unit test for PrivateRoute

---
+++ src/components/PrivateRoute.test.tsx
@@ -0,0 +1,40 @@
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import PrivateRoute from './PrivateRoute';
+import { useUserData } from 'hooks/useUserData';
+
+import type { Mock } from 'vitest';
+
+vi.mock('hooks/useUserData');
+
+const MockView = () => <div>Mock View</div>;
+
+describe('PrivateRoute', () => {
+  it('renders the private route', () => {
+    (useUserData as Mock).mockReturnValue({
+      isFetching: true,
+      isLoggedIn: false,
+    });
+
+    render(
+      <MemoryRouter>
+        <PrivateRoute using={MockView} />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Mock View')).toBeInTheDocument();
+  });
+
+  it('redirects home if user is not logged in', () => {
+    (useUserData as Mock).mockReturnValue({
+      isFetching: false,
+      isLoggedIn: false,
+    });
+
+    render(
+      <MemoryRouter>
+        <PrivateRoute using={MockView} />
+      </MemoryRouter>
+    );
+    expect(screen.queryByText('Mock View')).toBeNull();
+  });
+});
