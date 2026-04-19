44695bff51a49402983c49ffc7b7f6c85a2af1db
Enrico Lamperti <elamperti@users.noreply.github.com>
2026-04-19 12:05:14 -0300

    Fix lazy imports breaking e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,30 @@
+import { CypressEsm } from '@cypress/vite-plugin-cypress-esm';
 import { defineConfig } from 'cypress';
+import { mergeConfig } from 'vite';
+
+import viteConfig from './vite.config.mjs';
+
+import type { UserConfig } from 'vite';
 
 export default defineConfig({
   projectId: 'b19z84',
   e2e: {
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+    async setupNodeEvents(on, config) {
+      return (await import('./cypress/plugins/index.js')).default(on, config);
     },
     baseUrl: 'http://127.0.0.1:3030',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     experimentalRunAllSpecs: true,
+  },
+  component: {
+    devServer: {
+      bundler: 'vite',
+      framework: 'react',
+      viteConfig: () => {
+        return mergeConfig(viteConfig as UserConfig, {
+          plugins: [CypressEsm()],
+        });
+      },
+    },
   },
 });
